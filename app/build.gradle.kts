506aedbb769b23696c930946c3030f6673cc95c3
Mazhar <mazhartuhin8291@gmail.com>
2025-09-09 06:42:25 +0600

    project set up with dependency and plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("org.jetbrains.kotlin.plugin.serialization")
+    id("kotlin-parcelize")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -16,6 +22,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -28,14 +37,24 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "1.8"
     }
     buildFeatures {
         compose = true
+    }
+
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
@@ -56,4 +75,34 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.core.splashscreen)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
+
+//    implementation("androidx.credentials:credentials:1.3.0")
+//    implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
+//    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+//
+//    implementation("com.facebook.android:facebook-login:17.0.0")
+//    implementation("io.coil-kt.coil3:coil-compose:3.0.4")
+//    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
+//    implementation("com.google.maps.android:maps-compose:6.1.1")
+//    implementation("com.google.android.gms:play-services-location:21.0.1")
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
+//    implementation("com.stripe:stripe-android:20.53.0")
+//    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+//    implementation("com.google.firebase:firebase-messaging-ktx")
+//    implementation("com.google.maps.android:android-maps-utils:3.8.2")
+
+
+}
+kapt {
+    correctErrorTypes = true
 }
